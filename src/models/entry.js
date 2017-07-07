6287af679a42a5dfd55aebd7e5559f6e610404e6
David Keimes <david.keimes@hs-duesseldorf.de>
2017-07-07 16:31:18 +0200

    First commit

---
+++ src/models/entry.js
@@ -0,0 +1,7 @@
+class Entry {
+    constructor(name, content, journal){
+        this.name = name;
+        this.content = content;
+        this.journal = journal;
+    }
+}
---
+++ src/models/journal.js
@@ -0,0 +1,6 @@
+class Journal {
+    constructor(name, img_url){
+        this.name = name;
+        this.img_url = img_url;
+    }
+}
